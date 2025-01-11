ab4b94ed1fc6726bf15f19b45a59efd2edb6e55a
Yang-guoyong <106673025+Yang-guoyong@users.noreply.github.com>
2025-01-11 21:11:32 +0800

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -4,12 +4,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import streamlit as st
 from matplotlib import font_manager
+font_manager.fontManager.addfont('fonts/simfang.ttf')
 
 # 全局设置
 np.set_printoptions(precision=4)
 plt.rcParams['axes.unicode_minus'] = False
-plt.rcParams['font.family'] = ['Arial', 'KaiTi', 'SimSun', 'SimHei']
-
+#plt.rcParams['font.family'] = ['Arial', 'KaiTi', 'SimSun', 'SimHei']
+plt.rcParams['font.family'] = ['FangSong']
 # 输入数据
 pumping_rate = 528 / 1440  # m³/min
 obs_dist = 90              # m
@@ -43,17 +44,17 @@ i, j = data_limits
 beta, residuals = calc_beta(obs_time, drawdown, i, j)
 
 # 绘制观测值和拟合曲线
-ax.set(xscale="log", xlabel=r'$\lg t$', ylabel=r'$s$', title="Jacob fit(Least Squares)")
+ax.set(xscale="log", xlabel=r'$\lg t$', ylabel=r'$s$', title="最小二乘法-Jacob公式")
 ax.grid(True)
 ax.grid(True, which="major", linestyle="-", linewidth=0.5)
 ax.grid(True, which="minor", linestyle="-", linewidth=0.2)
-ax.scatter(obs_time, drawdown, label="obs", color="r", marker="*")
+ax.scatter(obs_time, drawdown, label="观测值", color="r", marker="*")
 ax.plot(
     obs_time,
     beta[0] + beta[1] * np.log10(obs_time),
-    label="line fit", linestyle="--", color="g"
+    label="拟合曲线", linestyle="--", color="g"
 )
-ax.legend(loc=4)
+ax.legend(loc=4, title="图例")
 
 # 计算并显示结果
 T = 0.183 * pumping_rate / beta[1]
